f176fcb64a2cf81ebe6461624ae566e19fd70269
desktopsupport <desktopsupport@opendealerexchange.com>
2025-01-27 15:15:41 -0500

    backup.bat

--- backup.bat
+++ backup.bat
@@ -39,3 +39,43 @@ if not exist "%Onedrive%\Backup\Chrome" mkdir "%Onedrive%\Backup\Chrome"
 XCopy "%BChrome%\Bookmarks*" "%Onedrive%\Backup\Chrome\Bookmarks*" /E /Y /C /Z /D >> "%Onedrive%\backup\log.txt"
 if not exist "%OneDriveCommercial%\Backup\Chrome" mkdir "%OneDriveCommercial%\Backup\Chrome"
 XCopy "%BChrome%\Bookmarks*" "%OneDriveCommercial%\Backup\Chrome\Bookmarks*" /E /Y /C /Z /D >> "%OneDriveCommercial%\backup\log.txt"
+
+::GET EDGE BOOKMARKS
+echo CopyingEdgeBookmarks >> "%Onedrive%\backup\log.txt"
+echo CopyingEdgeBookmarks >> "%OneDriveCommercial%\backup\log.txt"
+SET BEdge=%LOCALAPPDATA%\Microsoft\Edge\User Data\Default
+SET REdge="%Onedrive%\Backup\Edge"
+if not exist "%Onedrive%\Backup\Edge" mkdir "%Onedrive%\Backup\Edge"
+XCopy "%BEdge%\Bookmarks*" "%Onedrive%\Backup\Edge\Bookmarks*" /E /Y /C /Z /D >> "%Onedrive%\backup\log.txt"
+if not exist "%OneDriveCommercial%\Backup\Edge" mkdir "%OneDriveCommercial%\Backup\Edge"
+XCopy "%BEdge%\Bookmarks*" "%OneDriveCommercial%\Backup\Edge\Bookmarks*" /E /Y /C /Z /D >> "%OneDriveCommercial%\backup\log.txt"
+
+::GET FIREFOX BOOKMARKS
+echo CopyingFirefoxBookmarks >> "%Onedrive%\backup\log.txt"
+echo CopyingFirefoxBookmarks >> "%OneDriveCommercial%\backup\log.txt"
+SET BFirefox=%APPDATA%\Mozilla\Firefox\Profiles
+SET RFirefox="%Onedrive%\Backup\Firefox"
+if not exist "%Onedrive%\Backup\Firefox" mkdir "%Onedrive%\Backup\Firefox"
+XCopy "%BFirefox%\*.default*\places.sqlite" "%Onedrive%\Backup\Firefox\" /E /Y /C /Z /D >> "%Onedrive%\backup\log.txt"
+if not exist "%OneDriveCommercial%\Backup\Firefox" mkdir "%OneDriveCommercial%\Backup\Firefox"
+XCopy "%BFirefox%\*.default*\places.sqlite" "%OneDriveCommercial%\Backup\Firefox\" /E /Y /C /Z /D >> "%OneDriveCommercial%\backup\log.txt"
+
+::GET BRAVE BOOKMARKS
+echo CopyingBraveBookmarks >> "%Onedrive%\backup\log.txt"
+echo CopyingBraveBookmarks >> "%OneDriveCommercial%\backup\log.txt"
+SET BBrave=%LOCALAPPDATA%\BraveSoftware\Brave-Browser\User Data\Default
+SET RBrave="%Onedrive%\Backup\Brave"
+if not exist "%Onedrive%\Backup\Brave" mkdir "%Onedrive%\Backup\Brave"
+XCopy "%BBrave%\Bookmarks*" "%Onedrive%\Backup\Brave\Bookmarks*" /E /Y /C /Z /D >> "%Onedrive%\backup\log.txt"
+if not exist "%OneDriveCommercial%\Backup\Brave" mkdir "%OneDriveCommercial%\Backup\Brave"
+XCopy "%BBrave%\Bookmarks*" "%OneDriveCommercial%\Backup\Brave\Bookmarks*" /E /Y /C /Z /D >> "%OneDriveCommercial%\backup\log.txt"
+
+::GET SICKYNOTES
+echo CopyingStickyNotes >> "%Onedrive%\backup\log.txt"
+echo CopyingStickyNotes >> "%OneDriveCommercial%\backup\log.txt"
+SET BSticky=%APPDATA%\Microsoft\Sticky Notes
+SET RSticky="%Onedrive%\Backup\Sticky"
+if not exist "%Onedrive%\Backup\Sticky" mkdir "%Onedrive%\Backup\Sticky"
+XCopy /s "%BSticky%" "%Onedrive%\Backup\Sticky" /E /Y /C /Z /D >> "%Onedrive%\backup\log.txt"
+if not exist "%OneDriveCommercial%\Backup\Sticky" mkdir "%OneDriveCommercial%\Backup\Sticky"
+XCopy /s "%BSticky%" "%OneDriveCommercial%\Backup\Sticky" /E /Y /C /Z /D >> "%OneDriveCommercial%\backup\log.txt"
